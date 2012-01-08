88f356e935a29eb428f4d43e15e8aa9a8e1e3adb
marcus kamps <marcus.kamps@gmail.com>
2012-01-08 17:19:42 -0500

    Refactored main class
    Some uncommitted changes I made a while ago, so I don't remember every
    change, but the action has been split up into a few separate methods,
    and a few updates made.

--- src/world.py
+++ src/world.py
@@ -5,6 +5,7 @@ from PIL import Image
 class World:
     def __init__(self, path):
         
+        # load tile properties
         self.xtiles = []
         with open('tiles.csv', 'rb') as tiles:
             for line in tiles.readlines():
@@ -13,121 +14,150 @@ class World:
                     self.xtiles.append(int(id))
                     
         with open(path, 'rb') as self.file:
-            level = {}
-            for key, type in [
-                              ('version', 'dword'),
-                              ('title', 'pstring'),
-                              ('id', 'dword'),
-                              ('bounds', 'rect'),
-                              ('height', 'dword'),
-                              ('width', 'dword'),
-                              ('spawnx', 'dword'),
-                              ('spawny', 'dword'),
-                              ('groundlevel', 'double'),
-                              ('rocklevel', 'double'),
-                              ('time', 'double'),
-                              ('night', 'bool'),
-                              ('moonphase', 'dword'),
-                              ('bloodmoon', 'bool'),
-                              ('dungeonx', 'dword'),
-                              ('dungeony', 'dword'),
-                              ('boss1defeated', 'bool'),
-                              ('boss2defeated', 'bool'),
-                              ('boss3defeated', 'bool'),
-                              ('goblinsaved', 'bool'),
-                              ('wizardsaved', 'bool'),
-                              ('mechanicsaved', 'bool'),
-                              ('goblinsdefeated', 'bool'),
-                              ('clowndefeated', 'bool'),
-                              ('orbdestroyed', 'bool'),
-                              ('meteor', 'bool'),
-                              ('orbsdestroyed', 'byte'),
-                              ('altarsdestroyed', 'dword'),
-                              ('hardmode', 'bool'),
-                              ('goblintime', 'dword'),
-                              ('goblinsize', 'dword'),
-                              ('goblintype', 'dword'),
-                              ('goblinx', 'double')
-                              ]:
-                level[key] = self.read_data(type)
-                print key, level[key]
+            header = self.read_header()
+            tiles = self.read_tiles(header['width'], header['height'])
+            chests = self.read_chests()
+            signs = self.read_signs()
+            npcs = self.read_npcs()
+            npcnames = self.read_npcnames()
         
-            print 'reading tiles...'
-            tiles = {}
-            more = 0
-            for x in range(level['width']):
-                self.display_progress(x + 1, level['width'])
-                
-                for y in range(level['height']):
+        self.generate_image(tiles)
+
+            
+    def read_header(self):
+        header = {}
+        for key, type in [
+                          ('version', 'dword'),
+                          ('title', 'pstring'),
+                          ('id', 'dword'),
+                          ('bounds', 'rect'),
+                          ('height', 'dword'),
+                          ('width', 'dword'),
+                          ('spawnx', 'dword'),
+                          ('spawny', 'dword'),
+                          ('groundlevel', 'double'),
+                          ('rocklevel', 'double'),
+                          ('time', 'double'),
+                          ('night', 'bool'),
+                          ('moonphase', 'dword'),
+                          ('bloodmoon', 'bool'),
+                          ('dungeonx', 'dword'),
+                          ('dungeony', 'dword'),
+                          ('boss1defeated', 'bool'),
+                          ('boss2defeated', 'bool'),
+                          ('boss3defeated', 'bool'),
+                          ('goblinsaved', 'bool'),
+                          ('wizardsaved', 'bool'),
+                          ('mechanicsaved', 'bool'),
+                          ('goblinsdefeated', 'bool'),
+                          ('clowndefeated', 'bool'),
+                          ('orbdestroyed', 'bool'),
+                          ('meteor', 'bool'),
+                          ('orbsdestroyed', 'byte'),
+                          ('altarsdestroyed', 'dword'),
+                          ('hardmode', 'bool'),
+                          ('goblintime', 'dword'),
+                          ('goblinsize', 'dword'),
+                          ('goblintype', 'dword'),
+                          ('goblinx', 'double')
+                          ]:
+            header[key] = self.read_data(type)
+        return header
+
+        
+    def read_tiles(self, width, height):
+        print 'reading tiles...'
+        tiles = {}
+        more = 0
+        for x in range(width):
+            self.display_progress(x + 1, width)
+            for y in range(height):
+                if more:
+                    tiles[(x, y)] = tile
+                    more -= 1
                     
-                    if more:
-                        tiles[(x, y)] = tile
-                        more -= 1
+                else:
+                    tile = {}
+                    if self.read_data('bool'):
+                        tile['type'] = self.read_data('byte')
+                        if tile['type'] in self.xtiles:
+                            texu = self.read_data('word')
+                            texv = self.read_data('word')
+                            tile['tex'] = (texu, texv)
+                    if self.read_data('bool'):
+                        tile['walltype'] = self.read_data('byte')
+                    if self.read_data('bool'):
+                        tile['liquidlevel'] = self.read_data('byte')
+                        tile['lava'] = self.read_data('bool')
+                    if self.read_data('bool'):
+                        tile['wire'] = True
                         
-                    else:
-                        tile = {}
-                        if self.read_data('bool'):
-                            tile['type'] = self.read_data('byte')
-                            if tile['type'] in self.xtiles:
-                                texu = self.read_data('word')
-                                texv = self.read_data('word')
-                                tile['tex'] = (texu, texv)
-                        if self.read_data('bool'):
-                            tile['walltype'] = self.read_data('byte')
-                        if self.read_data('bool'):
-                            tile['liquidlevel'] = self.read_data('byte')
-                            tile['lava'] = self.read_data('bool')
-                        if self.read_data('bool'):
-                            tile['wire'] = True
+                    tiles[(x, y)] = tile
+                    more = self.read_data('word')
+                    
+        return tiles
+
                             
-                        tiles[(x, y)] = tile
-                        more = self.read_data('word')
-                                                
-            print 'generating image...'
-            image = Image.new('RGB', (level['width'], level['height']))
-            imagedata = []
-            for y in range(level['height']):
-                self.display_progress(y + 1, level['height'])
-                for x in range(level['width']):
-                    colour = (0, 0, 0) if 'type' in tiles[(x, y)] else (255, 255, 255)
-                    imagedata.append(colour)
-            image.putdata(imagedata)
-            print 'saving image...'
-            image.save('D:\\code\\python\\terramap\\img\\map.png')
+    def read_chests(self):            
+        chests = []
+        for chest in range(1000):
+            if self.read_data('bool'):
+                x = self.read_data('dword')
+                y = self.read_data('dword')
+                items = []
+                for i in range(20):
+                    count = self.read_data('byte')
+                    name = self.read_data('pstring') if count > 0 else ''
+                    prefix = self.read_data('byte') if count > 0 else 0
+                    items.append((count, name, prefix))
+                chests.append(((x, y), items))
+        return chests
+
             
-                                    
-            chests = []
-            for chest in range(1000):
-                if self.read_data('bool'):
-                    x = self.read_data('dword')
-                    y = self.read_data('dword')
-                    items = []
-                    for i in range(20):
-                        stack = self.read_data('byte')
-                        name = self.read_data('pstring') if stack > 0 else ''
-                        items.append((stack, name))
-                    chests.append(((x, y), items))
-            print chests
-            
-            signs = []
-            for sign in range(1000):
-                if self.read_data('bool'):
-                    text = self.read_data('pstring')
-                    x = self.read_data('dword')
-                    y = self.read_data('dword')
-                    signs.append(((x, y), text))
-            print signs
-            
-            npcs = []
-            while self.read_data('bool'):
-                name = self.read_data('pstring')
-                x = self.read_data('float')
-                y = self.read_data('float')
-                homeless = self.read_data('bool')
-                homex = self.read_data('dword')
-                homey = self.read_data('dword')
-                npcs.append((name, (x, y), homeless, (homex, homey)))
-            print npcs
+    def read_signs(self):
+        signs = []
+        for sign in range(1000):
+            if self.read_data('bool'):
+                text = self.read_data('pstring')
+                x = self.read_data('dword')
+                y = self.read_data('dword')
+                signs.append(((x, y), text))
+        return signs
+
+
+    def read_npcs(self):        
+        npcs = []
+        while self.read_data('bool'):
+            name = self.read_data('pstring')
+            x = self.read_data('float')
+            y = self.read_data('float')
+            homeless = self.read_data('bool')
+            homex = self.read_data('dword')
+            homey = self.read_data('dword')
+            npcs.append((name, (x, y), homeless, (homex, homey)))
+        return npcs
+
+
+    def read_npcnames(self):        
+        npcnames = {}
+        for npc in ['merchant', 'nurse', 'armsdealer', 'dryad', 'guide', 'clothier', 'demolitionist', 'tinkerer', 'wizard', 'mechanic']:
+            npcnames[npc] = self.read_data('pstring')
+        return npcnames
+    
+    
+    def generate_image(self, tiles):
+        print 'generating image...'
+        image = Image.new('RGB', (level['width'], level['height']))
+        imagedata = []
+        for y in range(level['height']):
+            self.display_progress(y + 1, level['height'])
+            for x in range(level['width']):
+                colour = (0, 0, 0) if 'type' in tiles[(x, y)] else (255, 255, 255)
+                imagedata.append(colour)
+        image.putdata(imagedata)
+        print 'saving image...'
+        image.save('D:\\code\\python\\terramap\\img\\map.png')
+        print 'done.'
             
             
     def display_progress(self, divisor, dividend, interval=10):
